d689f44542ccf8c435d92cf7bf5c16173a8a4784
AlbertoDiLeo <115349758+AlbertoDiLeo@users.noreply.github.com>
2024-11-25 15:56:09 +0100

    Aggiunta del metodo combine per moltiplicare gli elementi di un array per la somma di un altro array, aggiunta del main e aggiornamento della documentazione Javadoc

--- src/main/java/it/unimi/di/prog2/e06/CombineClient.java
+++ src/main/java/it/unimi/di/prog2/e06/CombineClient.java
@@ -33,6 +33,9 @@ public class CombineClient {
 
   /**
    * Decodifica una stringa contenente interi separati da spazi.
+   * 
+   * REQUIRES: La stringa in ingresso non può essere null e deve contenere interi separati da spazi
+   * EFFECTS: Restituisce un array contenente gli interi presenti nella stringa.
    *
    * @param string la stringa in ingresso, non può essere {@code null} e deve contenere interi
    *     separati da spazi.
@@ -48,10 +51,57 @@ public class CombineClient {
     return result;
   }
 
+  //moltiplica ciascun elemento di a per la somma degli elementi di b; 
+  //ad esempio, se a = [1, 2, 3] e b = [4, 5], allora al ritorno a = [9, 18, 27]. 
+  //Cosa dovrebbe fare questa procedura se a o b sono nulli o vuoti? 
+  //Fornisci una specifica per combine che risponda a queste domande e 
+  //spiega perché la tua specifica è buona.
+
+  /**
+   * Moltiplica ciascun elemento di {@code a} per la somma degli elementi di {@code b}.
+   * 
+   * REQUIRES: {@code a} e {@code b} non possono essere nulli e devono contenere almeno un elemento.
+   * MODIFIES: {@code a}
+   * EFFECTS: Moltiplica ciascun elemento di {@code a} per la somma degli elementi di {@code b}.
+   * 
+   * // no eccezioni perchè sto assumendo che a e b non siano nulli e non vuoti (clausola REQUIRES)
+   * @param a l'array di interi da moltiplicare
+   * @param b l'array di interi di cui calcolare la somma
+   * 
+   */
+  public static void combine(int[] a, int[] b) {
+    int sumB = 0;
+    for (int value : b) sumB += value;
+    for (int i = 0; i < a.length; i++) a[i] *= sumB;
+  }
+    
+
   // Il main di questa classe legge due righe dal flusso di ingresso ciascuna
   // delle quali contiene gli interi (separati da spazi) di uno dei due array da
   // combinare e ne emette il risultato della combinazione (separando gli interi
   // uno per linea). Può avvalersi della funzione precedente per decodificare
   // ciascuna delle due linee in ingresso.
+
+  /**
+   * Legge due righe dal flusso di ingresso ciascuna delle quali contiene gli interi (separati da
+   * spazi) di uno dei due array da combinare e ne emette il risultato della combinazione (separando
+   * gli interi uno per linea).
+   * 
+   * REQUIRE: Il flusso di ingresso deve contenere due righe, ciascuna delle quali contiene interi
+   * MODIFIES: Il flusso di uscita
+   * EFFETS: Legge due righe dal flusso di ingresso ciascuna delle quali contiene gli interi
+   *    (separati da spazi) di uno dei due array da combinare e ne emette il risultato della
+   *   combinazione (separando gli interi uno per linea).
+   * @param args gli argomenti della riga di comando, non utilizzati
+   */
+
+  public static void main(String[] args) {
+    try (Scanner s = new Scanner(System.in)) {
+      int[] a = parseInts(s.nextLine());
+      int[] b = parseInts(s.nextLine());
+      combine(a, b);
+      for (int value : a) System.out.println(value);
+    }
+  }
 
 }
